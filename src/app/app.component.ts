8e689f35c845309b62eb66e3310f398dadc59048
niyi-16 <dasil.adam@gmail.com>
2026-01-19 01:24:07 -0400

    Styleed main page with tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
+import { myInput} from '../components/input.component';
+import { TasksComponent} from "../components/tasks.component";
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [myInput, RouterOutlet, RouterLink],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <main class="flex flex-col h-full ">
+      <!--Nav section-->
+      <div class="ng-gradient h-10">
+        <p class="text-center">ANGULAR TO DO LIST.</p>
+      </div>
+      
+      <!--main body-->
+      <section class="flex-1 place-items-center bg-blend-color">
+        <div class="flex outline-1 w-3/7 shadow-xl mt-6">
+          <div routerLink="" 
+              class="w-1/2 text-center p-3 outline bg-neutral-200 hover:cursor-pointer hover:brightness-50">TASKS</div>
+          <div routerLink="deleted" 
+              class="w-1/2 text-center p-3 outline bg-red-200 hover:cursor-pointer hover:brightness-50">DELETED</div>
+        </div>  
+        
+        <div class="mt-6 outline-1 w-3/7 p-3 shadow-xl shadow-gray-600/100 ">
+          <inputField class="flex flex-col justify-evenly w-full p-2"/>
+          <router-outlet />
+        </div>
+      </section>
+    </main>
 
-    
-  `,
-  styles: [],
+    <style>
+      
+      .ng-gradient{
+        background: linear-gradient(135deg, #DD0031 0%, #E2324D 50%, #591c61 100%,#070606 25%);
+      }
+     
+    </style>
+  ` ,
 })
 export class AppComponent {
   title = 'to-do-list';
