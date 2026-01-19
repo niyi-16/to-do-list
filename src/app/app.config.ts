4efd1ecb05b314e9ac7623c8b2ca7c6192984b9f
niyi-16 <dasil.adam@gmail.com>
2026-01-19 01:22:05 -0400

    added routing to switch between tasks and deleted tasks

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {provideRouter, Route, Routes} from '@angular/router';
+import {TasksComponent, TaskDetetedComponent} from "../components/tasks.component";
+
+const taskRoute:Route = {path: '', component: TasksComponent}
+const deletedRoute:Route = {path: 'deleted', component: TaskDetetedComponent}
+const routes:Routes = [taskRoute, deletedRoute]
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
