a9d9f3053952da1aac07d58bbcdbb655fc70dd38
niyi-16 <dasil.adam@gmail.com>
2026-01-20 19:44:27 -0400

    Refactored to use ng template for easier modification

--- src/components/tasks.component.ts
+++ src/components/tasks.component.ts
@@ -1,74 +1,68 @@
-import {Component, inject, OnInit} from "@angular/core";
+import {Component, inject} from "@angular/core";
 import {FormControl, ReactiveFormsModule} from "@angular/forms";
 import {TaskService} from "../services/task.service"
+import {NgTemplateOutlet} from "@angular/common";
 
 
+//Main components
 @Component({
     selector: "tasksComponent",
     standalone: true,
-    imports: [ReactiveFormsModule],
+    imports: [ReactiveFormsModule, NgTemplateOutlet],
     template: `
-         @if (tasks.length > 0) {
-              @for (task of tasks; track task.id) {
-
-                   <div class="task p-3 my-1.5 grid grid-cols-2 grid-rows-1"
-                        [class]="{completed :task.completed}">
-                        <!--Start edit-->
-                        @if (task.edit) {
-                             <input type="text" class="border col-start-1 row-start-1"
-                                    [formControl]="newName" [placeholder]="task.name">
-                             <input type="text" class="border col-start-1 row-start-2"
-                                    [formControl]="newDescription" [placeholder]="task.description">
-
-                             <div class="col-start-2 row-span-2 flex">
-
-                                  <button (click)="taskService.updateTask(task.id, {newName, newDescription})"
-                                          class="btn-hvr rounded-lg p-4 w-20 bg-blue-300">
-                                       Done
-                                  </button>
-                                  <button (click)="taskService.edit(task.id)"
-                                          class="btn-hvr rounded-lg p-4 w-20 bg-red-300">
-                                       Cancel
-                                  </button>
-                             </div>
-                        } @else {
-                             <h2 class="col-start-1 row-start-1">{{ task.name }}</h2>
-                             <p class="col-start-1 row-start-2">{{ task.description }}</p>
-
-                             <div class="col-start-2 row-span-2 flex w-2/3 justify-between place-self-center-safe">
-                                  <button (click)="taskService.completeTask(task.id)"
-                                          class="btn-hvr rounded-lg p-4 bg-green-300">
-                                       <img class="w-6" src="/edit.svg" alt="Done icon">
-                                  </button>
-                                  <button (click)="taskService.edit(task.id)"
-                                          class="btn-hvr rounded-lg p-4 bg-blue-300">
-                                       <img class="w-6" src="/details.svg" alt="Edit button">
-                                  </button>
-                                  <button (click)="taskService.deleteTask(task.id)"
-                                          class="btn-hvr rounded-lg p-4 bg-red-600">
-                                       <img class="w-6" src="/delete.svg" alt="Delete icon">
-                                  </button>
-                             </div>
-                        }
-                   </div>
-              }
+         @for (task of tasks; track task.id) {
+              <div class="task grid grid-cols-2 grid-rows-1 my-1.5 p-3" [class.completed]="task.completed">
+                   <ng-container *ngTemplateOutlet="task.edit ? editMode : viewMode; context: { $implicit: task }"/>
+              </div>
+         } @empty {
+              <p class="text-center text-gray-500">No tasks available.</p>
          }
+         
+         <!-- View Mode Template -->
+         <ng-template #viewMode let-task>
+              <div class="col-start-1">
+                   <h2 class="row-start-1">{{ task.name }}</h2>
+                   <p class="row-start-2">{{ task.description }}</p>
+              </div>
+              <div class="flex w-2/3 place-self-center-safe justify-between col-start-2 row-span-2">
+                   <button (click)="taskService.completeTask(task.id)" 
+                           class="btn-hvr rounded-lg bg-green-300 p-4">
+                        <img class="w-6" src="/edit.svg" alt="Done icon">
+                   </button>
+                   <button (click)="taskService.edit(task.id)" 
+                           class="btn-hvr rounded-lg bg-blue-300 p-4">
+                        <img class="w-6" src="/details.svg" alt="Edit button">
+                   </button>
+                   <button (click)="taskService.deleteTask(task.id)" 
+                           class="btn-hvr rounded-lg bg-red-600 p-4">
+                        <img class="w-6" src="/delete.svg" alt="Delete icon">
+                   </button>
+              </div>
+         </ng-template>
 
+         <!-- Edit Mode Template -->
+         <ng-template #editMode let-task>
+              <div class="grid col-start-1">
+                   <input type="text" class="border row-start-1" [formControl]="newName" [placeholder]="task.name">
+                   <input type="text" class="border row-start-2" [formControl]="newDescription" [placeholder]="task.description">
+              </div>
+              <div class="flex col-start-2 row-span-2">
+                   <button (click)="taskService.updateTask(task.id, {newName, newDescription})"
+                           class="btn-hvr w-20 rounded-lg bg-blue-300 p-4">Done
+                   </button>
+                   <button (click)="taskService.edit(task.id)" class="btn-hvr w-20 rounded-lg bg-red-300 p-4">Cancel</button>
+              </div>
+         </ng-template>
     `
 
 })
-export class TasksComponent implements OnInit{
+export class TasksComponent{
     taskService = inject(TaskService)
-
-    get tasks(){
-        return this.taskService.tasks
-    }
-
     newName = new FormControl();
     newDescription = new FormControl();
 
-    ngOnInit(){
-        this.taskService.loadTasks()
+    get tasks(){
+        return this.taskService.tasks
     }
 }
 
@@ -104,4 +98,3 @@ export class TaskDetetedComponent{
     }
 
 }
-
